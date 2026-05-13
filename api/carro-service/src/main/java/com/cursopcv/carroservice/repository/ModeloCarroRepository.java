b94b7c3845cba805ec1ccd621437753e133d5e5d
patriciabatistab <patricia02borges@gmail.com>
2026-05-13 16:00:08 -0300

    fix: ajuste no Jpa do ModeloCarroRepository
    - Mudança na entidade passada como argumento no Jpa do ModeloCarroRepository

--- api/carro-service/src/main/java/com/cursopcv/carroservice/repository/ModeloCarroRepository.java
+++ api/carro-service/src/main/java/com/cursopcv/carroservice/repository/ModeloCarroRepository.java
@@ -2,9 +2,10 @@ package com.cursopcv.carroservice.repository;
 
 import com.cursopcv.carroservice.model.Acessorio;
 import com.cursopcv.carroservice.model.Fabricante;
+import com.cursopcv.carroservice.model.ModeloCarro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ModeloCarroRepository extends JpaRepository<Fabricante,Integer> {
+public interface ModeloCarroRepository extends JpaRepository<ModeloCarro,Integer> {
 }
