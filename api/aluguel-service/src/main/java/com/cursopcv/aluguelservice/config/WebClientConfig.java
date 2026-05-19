9cde084a55e468bb6411c1c95ee53ca61338d3fe
patriciabatistab <patricia02borges@gmail.com>
2026-05-19 14:01:04 -0300

    fix: arrumando WebClientConfig somente com o Builder
    - config: WebClientConfig com Builder, retirado demais código

--- api/aluguel-service/src/main/java/com/cursopcv/aluguelservice/config/WebClientConfig.java
+++ api/aluguel-service/src/main/java/com/cursopcv/aluguelservice/config/WebClientConfig.java
@@ -1,6 +1,5 @@
 package com.cursopcv.aluguelservice.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -11,37 +10,5 @@ public class WebClientConfig {
     @Bean
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
-    }
-
-    @Value("${services.pessoa-service.url}")
-    private String pessoaServiceUrl;
-
-    @Value("${services.carro-service.url}")
-    private String carroServiceUrl;
-
-    @Value("${services.notification-service.url}")
-    private String notificationServiceUrl;
-
-    @Bean("pessoaWebClient")
-    public WebClient pessoaWebClient() {
-        return WebClient.builder()
-                .baseUrl(pessoaServiceUrl)
-                .build();
-    }
-
-
-
-    @Bean("carroWebClient")
-    public WebClient carroWebClient() {
-        return WebClient.builder()
-                .baseUrl(carroServiceUrl)
-                .build();
-    }
-
-    @Bean("notificationWebClient")
-    public WebClient notificationWebClient() {
-        return WebClient.builder()
-                .baseUrl(notificationServiceUrl)
-                .build();
     }
 }
